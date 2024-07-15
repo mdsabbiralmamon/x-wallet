1d844cf273d5fd7aed1f22024a96fb61f4cef939
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-07-15 23:41:43 +0600

    Updated: Added Auth Provider

--- x-wallet-client/src/main.jsx
+++ x-wallet-client/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Routes from './routes/Routes.jsx'
+import { RouterProvider } from 'react-router'
+import AuthProvider from './providers/AuthProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={Routes} />
+    </AuthProvider>
   </React.StrictMode>,
 )
