8f436a58561bcb88d02fb0ebcc6f6761015353f2
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-07-15 23:43:11 +0600

    Add: Created auth provider

---
+++ x-wallet-client/src/providers/AuthProvider.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      setUser({ email: "", password: "" });
+      setLoading(false);
+    };
+
+    fetchUserData();
+  }, []);
+
+  const authInfo = { user, loading };
+
+  return (
+    <AuthContext.Provider value={authInfo}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AuthProvider;
