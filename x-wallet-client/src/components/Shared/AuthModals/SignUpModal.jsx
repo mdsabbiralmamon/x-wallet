bc524625ffd5618bae2fbd6937628d3e8d87d60f
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-07-16 13:50:35 +0600

    Created: Modal

---
+++ x-wallet-client/src/components/Shared/AuthModals/SignUpModal.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types'
+
+const SignUpModal = ({ showModal, onClose, onSelect }) => {
+  if (!showModal) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-800 bg-opacity-50">
+      <div className="bg-white p-8 rounded shadow-md">
+        <h2 className="text-lg font-bold mb-4">Sign Up As</h2>
+        <button
+          className="btn btn-primary w-full mb-2"
+          onClick={() => onSelect('user')}
+        >
+          User
+        </button>
+        <button
+          className="btn btn-primary w-full mb-2"
+          onClick={() => onSelect('agent')}
+        >
+          Agent
+        </button>
+        <button className="btn btn-secondary w-full mt-4" onClick={onClose}>
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
+};
+
+SignUpModal.propTypes = {
+    showModal: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired,
+    onSelect: PropTypes.func.isRequired,
+};
+
+export default SignUpModal;
