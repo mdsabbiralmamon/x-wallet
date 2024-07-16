c5db8269480c3b07c2378430976cfb8ace440dd7
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-07-16 23:52:44 +0600

    Updated: Add sign out functionality to Dashboard page

--- x-wallet-client/src/pages/Dashboard/Dashboard.jsx
+++ x-wallet-client/src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Dashboard = () => {
+    const {signOut} = useContext(AuthContext);
     return (
         <div>
             <h2>dashboard</h2>
+            <button className="btn btn-primary" onClick={signOut}> sign out </button>
         </div>
     );
 };
