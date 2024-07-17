f44af9db563d5ff0c404dca64500bdd857273600
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-07-18 01:40:11 +0600

    Updated:  CORS configuration for x-wallet-server

--- x-wallet-server/index.js
+++ x-wallet-server/index.js
@@ -9,7 +9,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://x-wallet-bysam.firebaseapp.com",
+      "https://x-wallet-bysam.web.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // mongoDB
